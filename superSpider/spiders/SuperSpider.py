2a1a7aac014f242cf3c0d17fcf472808e28bb02b
Yoon JooSung <xelloss705@naver.com>
2016-01-14 02:21:31 +0900

    일단 첫페이지를 엑셀에 담는것 까지 성공했음
    1. 나머지 url잡아서 넣기 // 두 세번째 페이지꺼 url 페이징 받아놓기~!
    2. 쭉 돌린 후 excel 파일로 뿌리기
    3. cmdline 아니라 프로그램에서 할 수 있게 하기
    4. 날짜 조절해주기
    5. '파일 명(아마 날짜?) 물어보기'
    6. 인자 조절가능하게 ui
    7. 최적화해주기
    8. 주석달기 및 코드정리
    9. 다른페이지쪽도 고고

--- superSpider/spiders/SuperSpider.py
+++ superSpider/spiders/SuperSpider.py
@@ -8,10 +8,11 @@ import ssl
 import cookielib
 import sys,time,urllib, urllib2, os, socket,random
 import chardet
-from lxml import etree
-import StringIO
+
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
+
+import xlsxwriter
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -20,7 +21,7 @@ sys.setdefaultencoding('utf-8')
 class SuperSpider (scrapy.Spider):
     name = "super"
     allowed_domains = ["www.ggi.co.kr"]
-    base_url = "https://www.ggi.co.kr"
+    base_url = "http://www.ggi.co.kr"
     start_urls = ["https://www.ggi.co.kr/home1.asp"]
 
     cookie = ""
@@ -133,74 +134,101 @@ class SuperSpider (scrapy.Spider):
            # print res.headers.get('Set-Cookie')
 #            print self.cookie
 
+            searchResultHtml = res.read()
 
-            req = urllib2.Request("http://www.ggi.co.kr/common/mulgun_detail_popup2.asp?idcode=A1423E403E3E3F3E3D483E3D3F43473F483E3C6&resStartDate=20160113&new=new")
-            res = self.opener.open(req)
-            print res.url
-            #print res.read()
+            links = Selector(text=searchResultHtml).xpath('//a[contains(@href, "common/mulgun_detail_popup2")]/@href').extract()
+            links = list(set(links)) #remove duplicated data
+            sizeOfLinks = len(links)
 
-
-
-
-           # rawdata = res.read().decode('utf-8','ignore')
-           # html = rawdata
-
-            rawdata = res.read()
-            encoding = chardet.detect(rawdata)
-            html = rawdata.decode(encoding['encoding'])
-
-   # links = response.xpath('//a[contains(@href, "common/mulgun_detail_popup2")]/@href').extract()
-          #  print Selector(text=html).css('td[class="td1"]').extract()
-
-#http://www.dreamy.pe.kr/zbxe/CodeClip/163260
-#http://www.yangbeom.link/post/130613532096/python%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%ED%81%B4%EB%A6%AC%EC%95%99-%ED%8C%8C%EC%84%9C%EB%A7%8C%EB%93%A4%EA%B8%B0-beautifulsoup-%EC%82%AC%EC%9A%A9%ED%8E%B8
-
-            print "=======Beautiful soup Test======"
-            soup = BeautifulSoup(html,"html5lib") #you have install it with "pip install html5lib"
-           # find_mytr = soup.find_all("tr", attrs={'class':"td_1"})
-          #  print soup
-            print soup.title.get_text().encode('cp949','ignore') # refer from https://kldp.org/node/81708
-           # print soup.find_all(id="Table1")
-            #print soup.find_all("td", class_="td_1")
-            #print soup.select("#Table1 > tbody")
-            #print soup.select("#Table1 > tbody > tr")
-            #print soup.select("#Table1 > tbody > tr + tr")
-            #print soup.select("#Table1 > tbody > tr + tr > td")
-            #print soup.select('td[class="td_1"]')  # I find it!!
-            tdList = soup.select('td[class="td_1"]')
-            for td in tdList:
-                print td.get_text(strip=True).encode('cp949','ignore')
-
-
-            #print soup.select("#Table1 > tbody > tr + tr > td.td_1")
-            #//*[@id="Table1"]/tbody/tr[1]/td[2]
-            ##Table1 > tbody > tr:nth-child(1) > td.td_1  ## error reason : http://stackoverflow.com/questions/24720442/selecting-second-child-in-beautiful-soup-with-soup-select
-
-            #print find_mytr
-            #for t in find_mytr:
-            #    print t.get_text(strip=True).encode('cp949','ignore').decode('cp949')
-
-
-#            print "=======lxml etree xpath Test======"
- #           #parser = etree.HTMLParser()
-  #          #tree   = etree.parse(StringIO.StringIO(html), parser)
-   #         tree = etree.HTML(html)
-    #        print tree
-     #       print tree.xpath('//*[@id="Table2"]/tbody/tr[7]/td[2]/table[2]/tbody/tr[2]/td[4]/text()[1]')
+            for index, link in enumerate(links):
+                links[index] = link.replace("..",self.base_url)
+                #print links[index]
 
 
 
-          #  filenmae = 'test.html'
-          #  with open(filenmae, 'wb') as f:
-          #      f.write(soup.title.get_text())
+            itemsList = []
+
+            for linkIdx, link in enumerate(links):
+                req = urllib2.Request(link)
+                res = self.opener.open(req)
+                print res.url
+
+                rawdata = res.read()
+                encoding = chardet.detect(rawdata)
+                html = rawdata.decode(encoding['encoding'])
+
+       # links = response.xpath('//a[contains(@href, "common/mulgun_detail_popup2")]/@href').extract()
+              #  print Selector(text=html).css('td[class="td1"]').extract()
+
+    #http://www.dreamy.pe.kr/zbxe/CodeClip/163260
+    #http://www.yangbeom.link/post/130613532096/python%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%ED%81%B4%EB%A6%AC%EC%95%99-%ED%8C%8C%EC%84%9C%EB%A7%8C%EB%93%A4%EA%B8%B0-beautifulsoup-%EC%82%AC%EC%9A%A9%ED%8E%B8
+
+                print "=======Beautiful soup Parsing HTML"+str(linkIdx)+"/"+str(sizeOfLinks)+"======"
+                soup = BeautifulSoup(html,"html5lib") #you have install it with "pip install html5lib"
+               # find_mytr = soup.find_all("tr", attrs={'class':"td_1"})
+              #  print soup
+                #print soup.title.get_text().encode('cp949','ignore') # refer from https://kldp.org/node/81708
+                #itemLocNo = soup.title.get_text().encode('cp949','ignore').split()[0]
+                itemLocNo = soup.title.get_text().split()[0]
+               # print soup.find_all(id="Table1")
+                #print soup.find_all("td", class_="td_1")
+                #print soup.select("#Table1 > tbody")
+                #print soup.select("#Table1 > tbody > tr")
+                #print soup.select("#Table1 > tbody > tr + tr")
+                #print soup.select("#Table1 > tbody > tr + tr > td")
+                #print soup.select('td[class="td_1"]')  # I find it!!
+                tdList = soup.select('td[class="td_1"]')
+                #for td in tdList:
+                #    print td.get_text(strip=True).encode('cp949','ignore')
+                #itemLocation = tdList[0].get_text(strip=True).encode('cp949','ignore')
+                #itemName = tdList[4].get_text(strip=True).encode('cp949','ignore')
+                itemLocation = tdList[0].get_text(strip=True)
+                itemName = tdList[4].get_text(strip=True)
+                #print itemName
+                #print itemLocNo
+                #print itemLocation
+                items = [itemName, itemLocNo, itemLocation]
+
+                itemsList.append(items)
 
 
 
+            # Create an new Excel file and add a worksheet.
+            workbook = xlsxwriter.Workbook('demo.xlsx')
+            worksheet = workbook.add_worksheet()
+
+            # Widen the first column to make the text clearer.
+            worksheet.set_column('A:A', 20)
+
+            # Add a bold format to use to highlight cells.
+            bold = workbook.add_format({'bold': True})
+
+            # Write some simple text.
+            #worksheet.write('A1', 'Hello')
+
+            # Text with formatting.
+            #worksheet.write('A2', 'World', bold)
+            # Write some numbers, with row/column notation.
+
+            for rowidx, items in enumerate(itemsList):
+                for colindx, item in enumerate(items):
+                    worksheet.write(rowidx, colindx, item)
 
 
-         #   filenmae = 'test.html'
-         #   with open(filenmae, 'wb') as f:
-         #       f.write(html)
+            workbook.close()
+
+                #print soup.select("#Table1 > tbody > tr + tr > td.td_1")
+                #//*[@id="Table1"]/tbody/tr[1]/td[2]
+                ##Table1 > tbody > tr:nth-child(1) > td.td_1  ## error reason : http://stackoverflow.com/questions/24720442/selecting-second-child-in-beautiful-soup-with-soup-select
+
+                #print find_mytr
+                #for t in find_mytr:
+                #    print t.get_text(strip=True).encode('cp949','ignore').decode('cp949')
+
+
+             #   filenmae = 'test.html'
+             #   with open(filenmae, 'wb') as f:
+             #       f.write(html)
 
 
 
@@ -211,28 +239,18 @@ class SuperSpider (scrapy.Spider):
 
        # soup = BeautifulSoup(response.body, "html.parser") #lxml
         item = SuperspiderItem()
-
         print "itemNo"
         links = response.xpath('//a[contains(@href, "common/mulgun_detail_popup2")]/@href').extract()
-
         for index, link in enumerate(links):
             links[index] = link.replace("..",self.base_url)
-
         print links[0]
        # urls = soup.select('a')
        # print soup
         yield scrapy.Request(url='https://www.ggi.co.kr/common/mulgun_detail_popup2.asp?idcode=A1493E473E413F3E3E3E3E3D403D42463F3E3C6&resStartDate=20160111&new=new',
                               method='GET',
                               callback=self.parse_page2)
-        #check login session or cookie problem~!
-
-
-    def parse_page1(self, response):
-        return scrapy.Request("http://www.example.com/some_page.html",
-                              callback=self.parse_page2)
 
     def parse_page2(self, response):
-        # this would log http://www.example.com/some_page.html
         print response.url
       #  print response.body
         filenmae = 'test.html'
